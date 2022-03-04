c4d6d70725926e0e0b1b22254e27b28a47a9ccae
nikhil swami <nikhilswami1@Gmail.com>
2022-03-04 15:46:12 +0530

    PIT=[04-03-2022-15:46:12.47]

--- ~DEPLOY.cmd
+++ ~DEPLOY.cmd
@@ -11,7 +11,7 @@ echo %~dp0
 
 ::===CLOUD SYNC
 aws s3 sync . %s3path% --exclude "**.git\**" --exclude ".git/*" --exclude "*\node_modules\*" --delete
-%myssh% aws s3 sync %s3path% /srv/lipi/ --delete
+%myssh% aws s3 sync %s3path% /srv/lipi/ --exclude "*/node_modules/*" --delete
 
 
 %myssh% "bash -s " < RELOAD-NGINX.sh
