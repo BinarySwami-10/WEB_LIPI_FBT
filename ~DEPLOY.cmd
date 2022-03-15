a6d76df683db41af28606dee98398e3dfb1eb445
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2022-03-15 11:51:54 +0530

    hom

--- ~DEPLOY.cmd
+++ ~DEPLOY.cmd
@@ -4,13 +4,13 @@ echo %myssh%
 ::==COMMIT-NIGGA
 call ~COMMIT.cmd
 
-::===BUILD STUFF
+::==BUILD STUFF
 call ./www.crayonpapers.in/!BUILD-WEBSITE.cmd
 cd %~dp0
 echo %~dp0
 
-::===CLOUD SYNC
-aws s3 sync . %s3path% --exclude "**.git\**" --exclude ".git/*" --exclude "*\node_modules\*" --delete
+::==CLOUD SYNC
+aws s3 sync . %s3path% --exclude "*.git\*" --exclude "*.git/*" --exclude "*\node_modules\*" --delete
 %myssh% aws s3 sync %s3path% /srv/lipi/ --exclude "*/node_modules/*" --delete
 
 
