0a195c34fe58061d69f4cfae179cc17b1e8ccf77
nikhil swami <nikhilswami1@Gmail.com>
2022-03-19 13:29:00 +0530

    fastapi integration office

--- www.fastholidayz.com/main.py
+++ www.fastholidayz.com/main.py
@@ -1,21 +1,37 @@
+# SERVER SETUP
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import uvicorn
-import requests
+
 from api.EaseMyTrip import Auth
 from api.EaseMyTrip import EaseMyTrip
+
+
+# GENERAL IMPORTS
 import os
 import colorama
+import requests
+import modulex as mx
+
+# INITIALIZE
 colorama.init()
 app = FastAPI()
+app.mount("/", StaticFiles(directory="./dev/", html = True), name="site")
+
+
+@app.get("/",response_class=HTMLResponse)
+async def home():
+    return mx.fread('./dev/index.html')
 
 
 @app.get("/api")
-def home():
+def apihome():
     return {"Hello": os.getcwd()}
 
 
 @app.get('/api/fs')
-async def get_flights(src,dst):
+async def get_flights(src,dst,fromdate):
     url = "https://stagingapi.easemytrip.com/Flight.svc/json/FlightSearch"
     data = {
         "Adults": "1",
@@ -34,7 +50,7 @@ async def get_flights(src,dst):
         ],
         "FlightSearchDetails": [
             {
-                "BeginDate": "2022-03-17",
+                "BeginDate": fromdate,
                 "Origin": src,
                 "Destination": dst
             },
