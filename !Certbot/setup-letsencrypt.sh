f75c450a120501cd8e36449c8f929e704926cb29
nikhil swami <nikhilswami1@Gmail.com>
2022-03-24 16:08:55 +0530

    PIT=[24-03-2022-16:08:55.65]

--- !Certbot/setup-letsencrypt.sh
+++ !Certbot/setup-letsencrypt.sh
@@ -13,3 +13,6 @@ sudo certbot -i nginx -a manual --preferred-challenges dns -d *.crayonpapers.in 
 #UPLOAD NEW CERT
 sudo aws s3 sync "/etc/letsencrypt/" "s3://cloud-workspace/WEB_LIPI_FBT/\!Certbot/certificates/"
 sudo aws s3 cp "/etc/nginx/nginx.conf" "s3://cloud-workspace/WEB_LIPI_FBT/nginx.conf"
+
+#DOWNLOAD NEW CERTIFICATE
+aws s3 cp "s3://cloud-workspace/WEB_LIPI_FBT/nginx.conf" "nginx-new.conf" 
