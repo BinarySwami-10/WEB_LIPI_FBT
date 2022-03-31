592216906e50dabf49af4fc56289f04890a4d8db
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2022-03-31 11:31:34 +0530

    PIT=[31-03-2022-11:31:34.14]

--- !Certbot/setup-letsencrypt.sh
+++ !Certbot/setup-letsencrypt.sh
@@ -11,11 +11,13 @@ sudo yum install -y ./epel.rpm
 sudo certbot -i nginx -a manual --preferred-challenges dns -d *.crayonpapers.in -d crayonpapers.in -d *.crayonpapers.com -d crayonpapers.com -d *.fastholidayz.com -d fastholidayz.com
 
 #UPLOAD NEW CERT
-sudo aws s3 sync "/etc/letsencrypt/" "s3://cloud-workspace/WEB_LIPI_FBT/\!Certbot/certificates/"
+sudo aws s3 sync "/etc/letsencrypt/" "s3://cloud-workspace/WEB_LIPI_FBT/\!Certbot/letsencrypt/"
 sudo aws s3 cp "/etc/nginx/nginx.conf" "s3://cloud-workspace/WEB_LIPI_FBT/nginx.conf"
 
 #DOWNLOAD NEW CERTIFICATE
-sudo aws s3 sync "s3://cloud-workspace/WEB_LIPI_FBT/\!Certbot/certificates/" "/etc/letsencrypt/" 
+sudo aws s3 sync "s3://cloud-workspace/WEB_LIPI_FBT/\!Certbot/letsencrypt/" "/etc/letsencrypt/" 
 sudo aws s3 cp "s3://cloud-workspace/WEB_LIPI_FBT/nginx.conf" "/etc/nginx/nginx.conf" 
 
+# Download Local
+aws s3 sync "s3://cloud-workspace/WEB_LIPI_FBT/\!Certbot/letsencrypt/" ./letsencrypt
 aws s3 cp "s3://cloud-workspace/WEB_LIPI_FBT/nginx.conf" "nginx-new.conf" 
