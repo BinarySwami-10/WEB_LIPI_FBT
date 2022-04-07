fab4bbefd8a268b34b50b64acc69211e684a9142
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2022-04-07 22:10:20 +0530

    PIT=[07-04-2022-22:10:20.84]

--- test/all-url-test.py
+++ test/all-url-test.py
@@ -4,19 +4,21 @@ import requests.exceptions
 if __name__ == '__main__':
     domain = 'crayonpapers'
     tld = ['.com', '.in']
-    subdomains= ['www.','']
-    schemas=['http://','https://']
+    subdomains = ['www.', '']
+    schemas = ['http://', 'https://']
 
-    urls = [sch+sub+domain+t for sch in schemas for sub in subdomains for t in tld]
+    urls = [sch + sub + domain +
+            t for sch in schemas for sub in subdomains for t in tld]
 
     # [print(x) for x in urls]
 
     for u in urls:
-    	try:
-	        print(requests.get(u),u)
-    	except Exception as e:
-	        print(e.response, u)
-    		
+        try:
+            r = requests.get(u)
+            print(r, u, r.url)
+        except Exception as e:
+            print(e.response, u)
+
 '''
 [ 'args', 'characters_written', 'errno', 'filename', 'filename2', 'request', 'response', 'strerror', 'winerror', 'with_traceback']
 '''
