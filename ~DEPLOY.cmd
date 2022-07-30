38d60a88d9b39840b46d67e4ead3b53595f280d0
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2022-07-30 20:46:37 +0530

    PIT=[Sat 07/30/2022-20:46:37.83]

--- ~DEPLOY.cmd
+++ ~DEPLOY.cmd
@@ -1,17 +1,16 @@
 set myssh=ssh -i "%userprofile%\.ssh\fastbooktrip-master-key.pem" ec2-user@ec2-65-0-173-111.ap-south-1.compute.amazonaws.com 
 set s3path=s3://cloud-workspace/WEB_LIPI_FBT/
-echo VARIABLES HAVE BEEN SET!
 
-
-::==COMMIT-NIGGA
-git add -A
-git commit -m "PIT=[%DATE%-%TIME%]"
-git push
 
 ::==BUILD STUFF
 call ./www.crayonpapers.in/!BUILD-WEBSITE.cmd
 cd %~dp0
 echo %~dp0
+
+::==COMMIT-NIGGA
+git add -A
+git commit -m "PIT=[%DATE%-%TIME%]"
+git push
 
 ::==CLOUD SYNC
 aws s3 sync . %s3path% --exclude "*.git\*" --exclude "*.git/*" --exclude "*\node_modules\*" 
